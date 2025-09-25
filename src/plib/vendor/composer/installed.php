120be7a04c8849f20f7597bdf3a91cdb6bf38d5d
Cristea Darius-Luca <luca.c@layershift.me>
2025-09-25 09:00:58 +0100

    Pushed composer's vendor/ directory

---
+++ src/plib/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'layershift/ext-ls-desec',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4a0c16ef5a464f3ba2f7b11cebec931bb78b84de',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'layershift/ext-ls-desec' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4a0c16ef5a464f3ba2f7b11cebec931bb78b84de',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
